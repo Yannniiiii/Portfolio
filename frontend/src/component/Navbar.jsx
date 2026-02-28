7dda46349f69916d05fafe1e81cccd6bbe5dc823
Yannniiiii <dnavarro0505@gmail.com>
2026-02-28 22:32:13 +0800

    Change Navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -88,6 +88,9 @@ export default function Navbar({ dark, setDark }) {
         "
       >
         <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+          <h1 className="text-xl font-bold tracking-wide text-white">
+            Deane's Portfolio
+          </h1>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center gap-8 text-white font-medium">
