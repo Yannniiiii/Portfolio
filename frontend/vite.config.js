e23ce900e81530cd033e95556806a900ac8fcbab
Yannniiiii <dnavarro0505@gmail.com>
2026-03-28 22:41:43 +0800

    config for design

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // ✅ important for Vercel deployment
+  plugins: [react(), tailwindcss()],
+  base: "/Portfolio/",
 });
